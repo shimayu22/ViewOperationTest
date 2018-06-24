60e8e88832f915acb8b5b35676f864b0f346b5ea
Yutaka Shimada <shimayu22@gmail.com>
2018-06-24 15:26:11 +0900

    ViewController以外からViewを操作しようとするとエラーになる？

---
+++ ViewOperationTest/LabelChange.swift
@@ -0,0 +1,18 @@
+//
+//  LabelChange.swift
+//  ViewOperationTest
+//
+//  Created by Yutaka Shimada on 2018/06/24.
+//  Copyright © 2018年 shimayu. All rights reserved.
+//
+
+import Foundation
+
+class labelChangeClass:NSObject {
+    
+    func labelTextChange(){
+        ViewController().LabelTest.text = "文字を変更したぞ"
+    }
+    
+    
+}
--- ViewOperationTest/ViewController.swift
+++ ViewOperationTest/ViewController.swift
@@ -10,9 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var LabelTest: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        labelChangeClass().labelTextChange()
+        
     }
 
     override func didReceiveMemoryWarning() {
